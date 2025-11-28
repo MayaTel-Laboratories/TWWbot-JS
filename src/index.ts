ebc28342750f20d3d65645e738040aa7eff30480
Maya Ventura <kingforpa@gmail.com>
2025-11-28 04:54:32 -0500

    add images, transfer atproto and image parsing code from esnupi.me bot with modified filename parsing

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,47 @@ import { postImage } from './clients/at';
 import { getNextImage } from './images';
 import * as dotenv from 'dotenv';
 dotenv.config();
+interface ImageDetails {
+  season: string;        // Y
+  episodeNumber: string; // ZZ
+  episodeTitle: string;  // Q
+  frameNumber: string;   // F
+}
+function parseImageName(imageName: string): ImageDetails | null {
+  const match = imageName.match(/^TWW_(\d)x(\d{2})_(.)__(\d+)\.jpeg$/);
 
-// EDIT THIS!
-function postTextFromImageName(imageName: string): string {
-  // Remove the file extension and parse the date
-  const dateParts = imageName.replace('.jpg', '').split('-');
-  const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
-
-  // Create a formatter
-  const formatter = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-
-  // Format the date
-  return formatter.format(date);
+  if (match) {
+    return {
+      season: match,
+      episodeNumber: match,
+      episodeTitle: match,
+      frameNumber: match,
+    };
+  }
+  return null;
 }
 
-// EDIT THIS!
-function altTextFromImageName(imageName: string): string {
-  return 'Image from ' + postTextFromImageName(imageName);
+function TextFromImageDetails(details: ImageDetails): string {
+  if (!details) {
+    return "A West Wing Image";
+  }
+  return `The West Wing - ${details.season}x${details.episodeNumber} - ${details.episodeTitle} - Frame ${details.frameNumber}`;
 }
-
-// Shouldn't have to edit this.
 async function main() {
   const { LAST_IMAGE_NAME: lastImageName } = process.env;
   const nextImage = await getNextImage({ lastImageName });
-
   console.log(nextImage.imageName);
+  const imageDetails = parseImageName(nextImage.imageName);
 
-  await postImage({
-    path: nextImage.absolutePath,
-    text: postTextFromImageName(nextImage.imageName),
-    altText: altTextFromImageName(nextImage.imageName),
-  });
+  if (imageDetails) {
+    const postText = TextFromImageDetails(imageDetails);
+    await postImage({
+      path: nextImage.absolutePath,
+      text: postText,
+      altText: postText,
+    });
+  } else {
+      console.error(`Could not parse image details from filename: ${nextImage.imageName}`);
+  }
 }
-
 main();
